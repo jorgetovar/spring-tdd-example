efd66d8e2097e8ec6dde8023931131993e52e8ac
jorgetovar <jorgetovar621@gmail.com>
2017-09-10 11:47:39 -0500

    test

--- src/test/java/com/jetprogramming/SpringTddApplicationTests.java
+++ src/test/java/com/jetprogramming/SpringTddApplicationTests.java
@@ -9,14 +9,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.jetprogramming.services.SumProvider;
 
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringTddApplicationTests {
-	
+
 	@Autowired
 	private SumProvider sumProvider;
-
 
 	@Test
 	public final void when2NumbersAreUsedThenNoExceptionIsThrown() {
@@ -28,12 +26,12 @@ public class SpringTddApplicationTests {
 	public final void whenNonNumberIsUsedThenExceptionIsThrown() {
 		sumProvider.add("1,X");
 	}
-	
+
 	@Test
 	public final void whenEmptyStringIsUsedThenReturnValueIs0() {
 		Assert.assertEquals(0, sumProvider.add(""));
 	}
-	
+
 	@Test
 	public final void whenOneNumberIsUsedThenReturnValueIsThatSameNumber() {
 		Assert.assertEquals(3, sumProvider.add("3"));
@@ -41,24 +39,24 @@ public class SpringTddApplicationTests {
 
 	@Test
 	public final void whenTwoNumbersAreUsedThenReturnValueIsTheirSum() {
-		Assert.assertEquals(3+6, sumProvider.add("3,6"));
+		Assert.assertEquals(3 + 6, sumProvider.add("3,6"));
 	}
-	
+
 	@Test
 	public final void whenAnyNumberOfNumbersIsUsedThenReturnValuesAreTheirSums() {
-		Assert.assertEquals(3+6+15+18+46+33, sumProvider.add("3,6,15,18,46,33"));
+		Assert.assertEquals(3 + 6 + 15 + 18 + 46 + 33, sumProvider.add("3,6,15,18,46,33"));
 	}
 
 	@Test
 	public final void whenNewLineIsUsedBetweenNumbersThenReturnValuesAreTheirSums() {
-		Assert.assertEquals(3+6+15, sumProvider.add("3,6\n15"));
+		Assert.assertEquals(3 + 6 + 15, sumProvider.add("3,6\n15"));
 	}
 
 	@Test
 	public final void whenDelimitedIsSpecifiedThenItIsUsedToSeparateNumbers() {
-		Assert.assertEquals(3+6+15, sumProvider.add("//;\n3;6;15"));
+		Assert.assertEquals(3 + 6 + 15, sumProvider.add("//;\n3;6;15"));
 	}
-	
+
 	@Test(expected = RuntimeException.class)
 	public final void whenNegativeNumberIsUsedThenRuntimeExceptionIsThrown() {
 		sumProvider.add("3,-6,15,18,46,33");
@@ -78,7 +76,7 @@ public class SpringTddApplicationTests {
 
 	@Test
 	public final void whenOneOrMoreNumbersAreGreaterThan1000IsUsedThenItIsNotIncludedInSum() {
-		Assert.assertEquals(3+1000+6, sumProvider.add("3,1000,1001,6,1234"));
+		Assert.assertEquals(3 + 1000 + 6, sumProvider.add("3,1000,1001,6,1234"));
 	}
 
 }
