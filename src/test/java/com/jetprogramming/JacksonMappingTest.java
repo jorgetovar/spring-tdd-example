01d34f3e42147235fbe001b2c19c0e8576b0b763
jorgetovar <jorgetovar621@gmail.com>
2017-09-10 11:39:01 -0500

    add circle config.yml

---
+++ src/test/java/com/jetprogramming/JacksonMappingTest.java
@@ -0,0 +1,14 @@
+package com.jetprogramming;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JacksonMappingTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
